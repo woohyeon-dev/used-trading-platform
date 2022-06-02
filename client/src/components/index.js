9747ed56ad5ff039c38407f874765b84c68714cf
woohyeon-dev <dngus3219@gmail.com>
2022-06-02 16:48:08 +0900

    Move the pagination component to the main folder

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,7 +3,7 @@ import Header from './Header';
 import HeaderBtn from './HeaderBtn';
 import Footer from './Footer';
 import Navigation from './Navigation';
-import Pagination from './Pagination';
+import Pagination from './main/Pagination';
 import BoardHeader from './main/board/BoardHeader';
 import BoardList from './main/board/BoardList';
 import Category from './main/market/Category';
