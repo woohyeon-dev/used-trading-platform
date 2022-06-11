803b383707a301f14f08f272075c744c7c20c9f3
woohyeon-dev <dngus3219@gmail.com>
2022-06-11 10:52:42 +0900

    Implement product posting

--- server/routes/market.js
+++ server/routes/market.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const { Product, User, Category } = require('../models/index');
-const { isLoggedIn } = require('./middlewares');
+const multer = require('multer');
 
 const router = express.Router();
 
-router.route('/product').get(async (req, res, next) => {
+// multer 옵션
+// hdd에 저장
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    // 저장위치 설정
+    //req: 요청정보  file : 업로드 파일 정보, cb: 업로드 설정완료시 호출
+    // public 폴더에 저장
+    cb(null, 'public/');
+  },
+  filename(req, file, cb) {
+    // 저장시 파일명 Date.now()_파일이름
+    cb(null, `${Date.now()}__${file.originalname}`);
+  },
+});
+
+// 미들웨어
+// 파일명을 보기 쉽게해서 관리하기 쉽게함
+const upload = multer({
+  storage: storage,
+  // 용량제한 10mb
+  limits: { fileSize: 10 * 1024 * 1024 },
+});
+
+router.get('/product', async (req, res, next) => {
   try {
-    console.log(req.query.cat_id);
     let catId = req.query.cat_id;
     let products;
     if (catId == 0) {
@@ -51,13 +73,14 @@ router.route('/product').get(async (req, res, next) => {
         cat_id: p.cat_id,
       });
     }
-    res.json(result);
+    return res.json(result);
   } catch (err) {
+    console.error(err);
     next(err);
   }
 });
 
-router.route('/category').get(async (req, res, next) => {
+router.get('/category', async (req, res, next) => {
   try {
     const categories = await Category.findAll({});
     const result = [];
@@ -68,17 +91,28 @@ router.route('/category').get(async (req, res, next) => {
         cat_desc: c.cat_desc,
       });
     }
-    res.json(result);
+    return res.json(result);
   } catch (err) {
+    console.error(err);
     next(err);
   }
 });
 
-router.route('/sell').post(async (req, res, next) => {
+router.post('/create', upload.single('image'), async (req, res, next) => {
   try {
-    console.log(req.body);
-    res.status(201).send('상품 판매 등록 완료!');
-  } catch (error) {
+    console.log('유저', req.user);
+    const { title, cat_id, price, descript } = req.body;
+    await Product.create({
+      title,
+      cat_id,
+      price,
+      descript,
+      image: req.file.filename,
+      writer: req.user,
+      // p_id 자동
+    });
+    return res.send('게시글이 작성되었습니다.');
+  } catch (err) {
     next(err);
   }
 });
